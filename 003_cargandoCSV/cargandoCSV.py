9e14519cdf86536a118870260f71c4b1d56add27
Ignacio <ign.gga@outlook.cl>
2023-08-21 12:22:30 -0400

    fomas de agregar datos a columas

--- 003_cargandoCSV/cargandoCSV.py
+++ 003_cargandoCSV/cargandoCSV.py
@@ -1,6 +1,8 @@
 def cargandoUnCSV():
     import pandas as pd
     datos = pd.read_csv('003_cargandoCSV\clientes.csv',encoding='latin-1',sep=';')
+    datos['NACIONALIDAD']='CHILE'#De esta forma cagregamps una columna nueva a nuetra tabla de datos.
+    datos['BONO'] = datos['MONTO']/datos['PUNTAJE_CREDITICIO']#Se genera una nueva columna a partir de la operacion entre dos columas, podriamos dar aqui una función para generar una variable especial
     print(datos)
     return(datos)
 cargandoUnCSV()
@@ -11,4 +13,4 @@ extraerFila=cargandoUnCSV().loc[658]#Con .loc(n) podemos revisar la fila de inte
 extrayendoFilas=cargandoUnCSV().loc[100:105]#Con .loc(m:n) podemos revisar el rango de filas de interes
 extraerValor=cargandoUnCSV().loc[658]['FECHA_NAC']#Con .loc[m][n], al igual que en una matriz, veremos el dato de la fila m y la columna n
 extraervalorFiltrado=cargandoUnCSV().loc[cargandoUnCSV()['TIPO_CLIENTE']=='A']#De esta forma llamara al dataframe y solo mostrara las filas que tengan ese valor en la columna indicada
-print(extrayendoFilas)
+print(extraervalorFiltrado)
