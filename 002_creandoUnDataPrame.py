29bd192ac6072f60d83536ca0eb9965a3a1a17b5
Ignacio <ign.gga@outlook.cl>
2023-08-21 11:35:53 -0400

    agregamos nombre a las columnas

--- 002_creandoUnDataPrame.py
+++ 002_creandoUnDataPrame.py
@@ -1,7 +1,7 @@
 def creandoUnDataFrame():
     import pandas as pd
     datos = [['Felipe',24,'Masculino',4.5],['Andrea',21,'Femenino',7.0],['Tomás',22,'Masculino',6.1],['Roberto',20,'Masculino',5.5]]
-    df = pd.DataFrame(datos)
+    df = pd.DataFrame(datos, columns=['Nombre','Edad','Genero','Calificación'])
     print(df)
 
 creandoUnDataFrame()
